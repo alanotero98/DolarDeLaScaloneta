30fe2b6b5dc27d47bfcec1d4abc657c96214d85e
Alan Otero <Oteroalan06@gmail.com>
2023-10-20 17:23:52 -0300

    primer commit

---
+++ src/DolarInfo.jsx
@@ -0,0 +1,44 @@
+// src/DolarInfo.js
+import React, { useState, useEffect } from 'react';
+import dolar from './img/dolar.jpg';
+import './DolarInfo.css';
+
+const DolarInfo = () => {
+  const [dolarInfo, setDolarInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dolarapi.com/v1/dolares/blue');
+        const data = await response.json();
+        setDolarInfo(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect will only run once
+
+  return (
+    <div className="dolar-info-container">
+      <div className="fondo-gif" />
+      <img className="foto-testa" src={dolar} alt="Tu Foto" />
+      <p className="descripcion">
+        ¡Hola! Soy Testac un tipo muy Kpo en finanzas y estoy aquí para contarte las últimas actualizaciones del dólar blue hoy:
+      </p>
+      {dolarInfo ? (
+        <div className="dolar-content">
+          <h2>Dólar Blue</h2>
+          <p>Información actualizada: {dolarInfo.fechaActualizacion}</p>
+          <p>Compra: {dolarInfo.compra}</p>
+          <p>Venta: {dolarInfo.venta}</p>
+        </div>
+      ) : (
+        <p>Cargando información...</p>
+      )}
+    </div>
+  );
+};
+
+export default DolarInfo;
